ac678fb4121fe225ed6397f5707a0627f0792faf
Felipe Morales <felipe.morales.querol@gmail.com>
2020-07-18 18:04:38 -0300

    fix para soporte de lint flake8

--- CuentaBancaria.py
+++ CuentaBancaria.py
@@ -1,28 +1,28 @@
 class cuentaBancaria():
-	saldo=0
-	movimientos=0
-	consultas_saldo=0
-	
-	def __init__(self, saldo):
-		self.movimientos += 1
-		self.saldo = saldo  
+    saldo = 0
+    movimientos = 0
+    consultas_saldo = 0
 
-	def depositar(self, aMount):
-		self.movimientos += 1
-		self.saldo = self.saldo + aMount
-		
-	def retirar(self, aMount):
-		#Condiciono a que tenga saldo
-		if self.saldo >= aMount:
-			self.movimientos += 1
-			self.saldo = self.saldo - aMount	
-		
-	def getSaldo(self):
-		self.consultas_saldo += 1
-		return self.saldo
+    def __init__(self, saldo):
+        self.movimientos += 1
+        self.saldo = saldo
 
-	def getCantMovimientos(self):
-		return self.movimientos
+    def depositar(self, aMount):
+        self.movimientos += 1
+        self.saldo = self.saldo + aMount
 
-	def getCantConsultasSaldo(self):
-		return self.consultas_saldo
+    def retirar(self, aMount):
+        # Condiciono a que tenga saldo
+        if self.saldo >= aMount:
+            self.movimientos += 1
+            self.saldo = self.saldo - aMount
+
+    def getSaldo(self):
+        self.consultas_saldo += 1
+        return self.saldo
+
+    def getCantMovimientos(self):
+        return self.movimientos
+
+    def getCantConsultasSaldo(self):
+        return self.consultas_saldo
--- tipoCuentas.py
+++ tipoCuentas.py
@@ -1,12 +1,11 @@
 class tipoCuenta():
 
-	def __init__(self, tipo, nombre):
-		self.tipo = tipo
-		self.nombre = nombre
+    def __init__(self, tipo, nombre):
+        self.tipo = tipo
+        self.nombre = nombre
 
-	def getTipo(self):
-		return self.tipo
+    def getTipo(self):
+        return self.tipo
 
-	def getNombre(self):
-		return self.nombre
-
+    def getNombre(self):
+        return self.nombre
