f8e4908c7db50d4bdf1eaec8fb36cc8c83c03a80
Felipe Morales <felipe.morales.querol@gmail.com>
2020-07-18 09:50:01 -0300

    actualizacion

--- testCuentaBancaria.py
+++ testCuentaBancaria.py
@@ -1,6 +1,7 @@
+#Autor: Felipe Morales
 #Fragmento de Sistema LK-ERP (modulo web bancario)
 import unittest
-from CuentaBancaria import cuentaBancaria
+from CuentaBancaria import cuentaBancaria, cuentaBancaria_CajaAhorro, cuentaBancaria_CuentaCorriente
 from tipoCuentas import tipoCuenta
 
 
@@ -9,6 +10,9 @@ class TestCuentaBancaria(unittest.TestCase):
 	def setUp(self):
 		self.cb1 = cuentaBancaria(0)
 		self.cb2 = cuentaBancaria(0)
+		self.cb_caja_ahorro = cuentaBancaria_CajaAhorro(0)
+		self.cb_cuenta_corriente = cuentaBancaria_CuentaCorriente(0)
+		
 
 	#Metodo que se ejecuta al finalizar cada caso para liberar recursos	
 	def tearDown(self):
@@ -17,15 +21,19 @@ class TestCuentaBancaria(unittest.TestCase):
 	def test_saldo_inicial(self):
 		self.assertEqual(self.cb1.getSaldo(), 0, "Debe mostrar 0")
 		self.assertEqual(self.cb1.getCantMovimientos(), 1, "Debe mostrar 1")
+		self.assertEqual(self.cb1.ultimo_error[0], 0, "Caja de Ahorro. Código de error 0")
 
 	def test_saldo_100(self):
 		self.cb1.depositar(100)
 		self.assertEqual(self.cb1.getSaldo(), 100, "Debe mostrar 100")
 		self.assertEqual(self.cb1.getCantMovimientos(), 2, "Debe mostrar 2")
+		self.assertEqual(self.cb1.ultimo_error[0], 0, "Caja de Ahorro. Código de error 0")
+		
 
 	def test_saldo_menos_100(self):
 		self.cb1.retirar(100)
 		self.assertEqual(self.cb1.getSaldo(), -100, "Debe mostrar 0")
+		self.assertEqual(self.cb1.ultimo_error[0], 0, "Caja de Ahorro. Código de error 0")
 		
 	def test_saldo_varios_movimiento(self):
 		self.cb1.retirar(100)
@@ -33,7 +41,8 @@ class TestCuentaBancaria(unittest.TestCase):
 		self.cb1.retirar(100)
 		self.cb1.depositar(100)
 		self.cb1.retirar(100)
-		self.assertEqual(self.cb1.getSaldo(), -100, "Debe mostrar 0")		
+		self.assertEqual(self.cb1.getSaldo(), -100, "Debe mostrar 0")
+		self.assertEqual(self.cb1.ultimo_error[0], 0, "Caja de Ahorro. Código de error 0")	
 
 	def test_saldo_tras_movimientos_entre_cuentas(self):
 		self.cb1.depositar(100)
@@ -44,7 +53,18 @@ class TestCuentaBancaria(unittest.TestCase):
 		self.assertEqual(self.cb1.getSaldo()-self.cb2.getSaldo(), 0, "Debe mostrar 0")
 		cant=self.cb1.getCantMovimientos()+self.cb2.getCantMovimientos()
 		self.assertEqual(cant, 7, "Debe mostrar 7 movimientos en total")
+		self.assertEqual(self.cb1.ultimo_error[0], 0, "Caja de Ahorro. Código de error 0")
+		self.assertEqual(self.cb2.ultimo_error[0], 0, "Caja de Ahorro. Código de error 0")
+	
+	def test_retiro_negativo_caja_ahorro(self):
+		self.cb_caja_ahorro.retirar(100)
+		self.assertEqual(self.cb_caja_ahorro.getSaldo(), 0, "Caja de Ahorro. Debe mostrar 0")
+		self.assertEqual(self.cb_caja_ahorro.ultimo_error[0], -1, "Caja de Ahorro. Código de error -1")
 
+	def test_retiro_negativo_cuenta_corriente(self):
+		self.cb_cuenta_corriente.retirar(100)
+		self.assertEqual(self.cb_cuenta_corriente.getSaldo(), -100, "Caja de Ahorro. Debe mostrar 0")
+		self.assertEqual(self.cb_cuenta_corriente.ultimo_error[0], 0, "Caja de Ahorro. Código de error ")
 
 if __name__ == '__main__':
 	unittest.main(verbosity=0)
