3afd148ed3dbe2dc894f4bc2fd355d7d91d3b1e7
Felipe Morales <felipe.morales.querol@gmail.com>
2020-07-22 18:35:51 -0300

    Mejoras para compatibilidad flake8

--- CuentaBancaria.py
+++ CuentaBancaria.py
@@ -1,25 +1,26 @@
-#Autor: Felipe Morales
+# Autor: Felipe Morales
 from tipoCuentas import tipoCuenta
+
+
 class cuentaBancaria():
-    #Tupla para registro de ultima operacion (en caso de error)
-    ultimo_error = (0,'')
+    # Tupla para registro de ultima operacion (en caso de error)
+    ultimo_error = (0, '')
     saldo = 0
     movimientos = 0
     consultas_saldo = 0
-    
+
     def __init__(self, saldo):
         self.movimientos += 1
-        self.saldo = saldo  
+        self.saldo = saldo
 
     def depositar(self, aMount):
         self.movimientos += 1
         self.saldo = self.saldo + aMount
 
-    
     def retirar(self, aMount):
         self.movimientos += 1
-        self.saldo = self.saldo - aMount	
-        
+        self.saldo = self.saldo - aMount
+
     def getSaldo(self):
         self.consultas_saldo += 1
         return self.saldo
@@ -30,15 +31,15 @@ class cuentaBancaria():
     def getCantConsultasSaldo(self):
         return self.consultas_saldo
 
+
 class cuentaBancaria_CajaAhorro(cuentaBancaria):
-    
+
     def __init__(self, saldo):
         super().__init__(saldo)
-        self.tc=tipoCuenta('A', 'Caja Ahorro $')
-    
-        
+        self.tc = tipoCuenta('A', 'Caja Ahorro $')
+
     def retirar(self, aMount):
-        #Condiciono a que tenga saldo
+        # Condiciono a que tenga saldo
         if self.saldo >= aMount:
             self.movimientos += 1
             self.saldo = self.saldo - aMount
@@ -47,13 +48,12 @@ class cuentaBancaria_CajaAhorro(cuentaBancaria):
 
 
 class cuentaBancaria_CuentaCorriente(cuentaBancaria):
-    
+
     def __init__(self, saldo):
         super().__init__(saldo)
-        self.tc=tipoCuenta('C', 'Cuenta Corriente $')
-     
-        
+        self.tc = tipoCuenta('C', 'Cuenta Corriente $')
+
     def retirar(self, aMount):
-        #Condiciono a que tenga saldo
+        # Condiciono a que tenga saldo
         self.movimientos += 1
         self.saldo = self.saldo - aMount
