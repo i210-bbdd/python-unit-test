fa4b6cb27bcbb1d0a9f278ee13b45674d9d52e95
Felipe Morales <felipe.morales.querol@gmail.com>
2020-07-27 23:16:01 -0300

    Agregado de max_operaciones en tipoCuentas

--- tipoCuentas.py
+++ tipoCuentas.py
@@ -1,10 +1,12 @@
 # Autor: Felipe Morales
+# Fecha: 26/07/2020
 class tipoCuenta():
 
-    def __init__(self, tipo, nombre, limite):
+    def __init__(self, tipo, nombre, limite, max_operaciones):
         self.tipo = tipo
         self.nombre = nombre
         self.limite = limite
+        self.max_operaciones = max_operaciones
 
     def get_tipo(self):
         return self.tipo
